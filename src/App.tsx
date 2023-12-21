b62229ce06df93139f071e3c8e01d48441adcc11
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-12-21 09:39:11 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Shopping Cart 🛒</h1>
+      </header>
+      <main>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+        fugiat minima quisquam velit optio repellendus tempore explicabo
+        corrupti quasi incidunt quo dolores ab tempora amet quibusdam, hic
+        tenetur reprehenderit quidem?
+      </main>
+    </>
+  )
+}
+
+export default App
